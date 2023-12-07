db2aa5aed9df99c0f02122b4481041bdf551bce4
Julie Tram Duong <65159924+trammmd@users.noreply.github.com>
2023-12-07 12:58:39 +0000

    Fix code

--- Praktikum-Sensor.py
+++ Praktikum-Sensor.py
@@ -1,36 +1,49 @@
 import paho.mqtt.client as mqtt
 import ssl
 
+# Create a new MQTT client instance
+client = mqtt.Client()
+
 # MQTT broker address and port
 broker_address = "http://mioty-server-1.iis.fraunhofer.de"
 broker_port = 8883
+
+# Connect to the MQTT broker
+client.connect(broker_address, broker_port)
 
 # MQTT credentials
 username = "stud"       
 password = "mioty$stud"       
 
+# Set the username and password (
+if username is not None and password is not None:
+    client.username_pw_set(username, password)
+    
 # MQTT topic to subscribe to
 topic = "devices/" + username + "/#"
 # TOPIC EXAMPLES
 # topic = devices/USERNAME/febris/+/+/up
-# topic = devices/USERNAME/apollon/+/+/up
 
 # MQTT topic to publish to (if required)
 publish_topic = "my/publish/topic"
 
-# QoS level to use when subscribing and publishing
 # QoS 0 (at most once): The message will be delivered at most once, and the broker will not send any confirmation that the message has been received.
 # QoS 2 (exactly once): The message will be delivered exactly once, and the broker will send a confirmation when the message has been received and processed.
-qos_level = 0
+qos_level = 2
 
-# Path to the client certificate and key files (if required)
+"""
+# Path to the client certificate and key files 
 cert_file = None
 key_file = None
 
-# Create an SSL context and load the client certificate and key files (if required)
+# Create an SSL context and load the client certificate and key files
 ssl_context = ssl.create_default_context()
 if cert_file is not None and key_file is not None:
-    ssl_context.load_cert_chain(cert_file, key_file)
+    ssl_context.load_cert_chain(cert_file, key_file)  
+    
+# Set the SSL context for secure connection
+client.tls_set_context(context=ssl_context)
+"""
 
 receivedMessage = ""
 
@@ -39,21 +52,8 @@ def on_message(client, userdata, message):
     receivedMessage = str(message.payload.decode())
     print("Received message: " + receivedMessage)
 
-# Create a new MQTT client instance
-client = mqtt.Client()
-
-# Set the username and password (if required)
-if username is not None and password is not None:
-    client.username_pw_set(username, password)
-
 # Set the callback function for new messages
 client.on_message = on_message
-
-# Set the SSL context for secure connection
-client.tls_set_context(context=ssl_context)
-
-# Connect to the MQTT broker
-client.connect(broker_address, broker_port)
 
 # Subscribe to the topic
 client.subscribe(topic, qos=qos_level)
