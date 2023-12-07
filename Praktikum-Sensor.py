9affa5a52d8fb9b275204f24527770654acaf99d
Julie Tram Duong <65159924+trammmd@users.noreply.github.com>
2023-12-07 15:19:00 +0000

    Fix Code

--- Praktikum-Sensor.py
+++ Praktikum-Sensor.py
@@ -5,12 +5,14 @@ import ssl
 client = mqtt.Client()
 
 # MQTT broker address and port
-broker_address = "http://mioty-server-1.iis.fraunhofer.de"
+broker_address = "mioty-server-1.iis.fraunhofer.de"
 broker_port = 8883
 
 # Connect to the MQTT broker
-client.connect(broker_address, broker_port)
-
+#client.connect(broker_address, broker_port)
+if client.connect(broker_address, broker_port) != 0:
+    print("Could not connect to MQTT broker!")
+    SystemExit(-1)
 # MQTT credentials
 username = "stud"       
 password = "mioty$stud"       
@@ -20,16 +22,16 @@ if username is not None and password is not None:
     client.username_pw_set(username, password)
     
 # MQTT topic to subscribe to
-topic = "devices/" + username + "/#"
-# TOPIC EXAMPLES
+topic = "devices/username/Air Quality (Sentinum Febris CO2)/febris/FC-A8-4A-03-00-00-0E-66/up"
+#topic = "devices/" + username + "/#"
 # topic = devices/USERNAME/febris/+/+/up
 
 # MQTT topic to publish to (if required)
-publish_topic = "my/publish/topic"
+#publish_topic = "my/publish/topic"
 
 # QoS 0 (at most once): The message will be delivered at most once, and the broker will not send any confirmation that the message has been received.
 # QoS 2 (exactly once): The message will be delivered exactly once, and the broker will send a confirmation when the message has been received and processed.
-qos_level = 2
+qos_level = 0
 
 """
 # Path to the client certificate and key files 
@@ -59,7 +61,14 @@ client.on_message = on_message
 client.subscribe(topic, qos=qos_level)
 
 # Publish a message to the topic with QoS 1
-client.publish(publish_topic, receivedMessage, qos=qos_level)
+#client.publish(publish_topic, receivedMessage, qos=qos_level)
 
 # Start the MQTT client loop to listen for new messages
-client.loop_forever()
+try:
+    print("Press CTRL+C to exit")
+    client.loop_forever()
+except:
+    print("Disconnecting from MQTT broker...")
+client.disconnect()
+   
+ 
